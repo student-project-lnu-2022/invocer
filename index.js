59119fc4deee127aea8e3416cc23c160419d3a54
ostapkilbasovych <ostapkilbasovych@gmail.com	ostapkilbasovych@MacBook-Pro-Ostap ~ %  git config --global user.email ostapkilbasovych@gmail.com>
2023-01-19 11:37:58 +0200

    M3 design items now are saved locally

---
+++ index.js
@@ -0,0 +1,4 @@
+import "@material/web/button/filled-button.js";
+import "@material/web/textfield/outlined-text-field.js";
+import "@material/web/fab/fab.js";
+import "@material/web/fab/fab-extended.js";
