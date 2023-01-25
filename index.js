ab99a438e48bb0cee196f603531c6a96445ff825
ostapkilbasovych <ostapkilbasovych@gmail.com	ostapkilbasovych@MacBook-Pro-Ostap ~ %  git config --global user.email ostapkilbasovych@gmail.com>
2023-01-25 20:41:01 +0200

    Added clients_list.html page

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ import "@material/web/button/filled-button.js";
 import "@material/web/textfield/outlined-text-field.js";
 import "@material/web/fab/fab.js";
 import "@material/web/fab/fab-extended.js";
+import '@material/web/checkbox/checkbox.js';
+
