4e8db2080b4d5ffc53a5f598085f3685dad395c3
ostapkilbasovych <ostapkilbasovych@gmail.com	ostapkilbasovych@MacBook-Pro-Ostap ~ %  git config --global user.email ostapkilbasovych@gmail.com>
2023-02-02 22:12:29 +0200

    Fixed registration validation bugs

--- invoicer_project/static/js/validate_registration.js
+++ invoicer_project/static/js/validate_registration.js
@@ -1,8 +1,68 @@
 function validateRegistration() {
+    removeMaxHeightAttribute();
     validate_email();
     validate_password();
     validate_name_and_surname();
+    checkAndSetNormalHeightNameAndSurnameInput();
+    checkAndSetNormalHeightPasswordAndRepeatPasswordInput();
 
+
+}
+
+function setNormalStateNameInput() {
+    document.getElementById("name_input_registration_page").removeAttribute("errorText");
+    document.getElementById("name_input_registration_page").removeAttribute("error");
+    removeMaxHeightAttribute();
+    checkAndSetNormalHeightNameAndSurnameInput();
+}
+
+function setNormalStateSurnameInput() {
+    document.getElementById("surname_input_registration_page").removeAttribute("errorText");
+    document.getElementById("surname_input_registration_page").removeAttribute("error");
+    removeMaxHeightAttribute();
+    checkAndSetNormalHeightPasswordAndRepeatPasswordInput();
+}
+
+function setNormalStatePasswordInput() {
+    checkAndSetNormalHeightPasswordAndRepeatPasswordInput();
+    document.getElementById("password_input_registration_page").removeAttribute("errorText");
+    document.getElementById("password_input_registration_page").removeAttribute("error");
+}
+
+function setNormalStateRepeatPasswordInput() {
+    checkAndSetNormalHeightPasswordAndRepeatPasswordInput();
+    document.getElementById("repeat_password_input_registration_page").removeAttribute("errorText");
+    document.getElementById("repeat_password_input_registration_page").removeAttribute("error");
+}
+
+function setNormalStateEmailInput() {
+    document.getElementById("email_input_registration_page").removeAttribute("errorText");
+    document.getElementById("email_input_registration_page").removeAttribute("error");
+}
+
+function checkAndSetNormalHeightNameAndSurnameInput() {
+    if (document.getElementById("name_input_registration_page").hasAttribute("error") && !(document.getElementById("surname_input_registration_page").hasAttribute("error"))) {
+        document.getElementById("surname_input_registration_page").style.maxHeight = "56px";
+    }
+    if (!(document.getElementById("name_input_registration_page").hasAttribute("error")) && document.getElementById("surname_input_registration_page").hasAttribute("error")) {
+        document.getElementById("name_input_registration_page").style.maxHeight = "56px";
+    }
+}
+
+function checkAndSetNormalHeightPasswordAndRepeatPasswordInput() {
+    if (document.getElementById("password_input_registration_page").hasAttribute("error") && !(document.getElementById("repeat_password_input_registration_page").hasAttribute("error"))) {
+        document.getElementById("repeat_password_input_registration_page").style.maxHeight = "56px";
+    }
+    if (!(document.getElementById("password_input_registration_page").hasAttribute("error")) && document.getElementById("repeat_password_input_registration_page").hasAttribute("error")) {
+        document.getElementById("password_input_registration_page").style.maxHeight = "56px";
+    }
+}
+
+function removeMaxHeightAttribute() {
+    document.getElementById("surname_input_registration_page").style.maxHeight = null;
+    document.getElementById("name_input_registration_page").style.maxHeight = null;
+    document.getElementById("password_input_registration_page").style.maxHeight = null;
+    document.getElementById("repeat_password_input_registration_page").style.maxHeight = null;
 }
 
 function validate_email() {
@@ -18,12 +78,10 @@ function validate_email() {
 
 function validate_password() {
     var user_password = document.getElementById("password_input_registration_page").value;
-    var user_password_repeat = document.getElementById("repeat_password_input_registration_page").value
+    var user_password_repeat = document.getElementById("repeat_password_input_registration_page").value;
     if (user_password_repeat !== user_password) {
         document.getElementById("repeat_password_input_registration_page").setAttribute("errorText", "Your passwords don't match");
         document.getElementById("repeat_password_input_registration_page").setAttribute("error", "true");
-        document.getElementById("password_input_registration_page").setAttribute("errorText", "Your passwords don't match");
-        document.getElementById("password_input_registration_page").setAttribute("error", "true");
     } else {
         document.getElementById("password_input_registration_page").removeAttribute("errorText");
         document.getElementById("password_input_registration_page").removeAttribute("error");
@@ -41,8 +99,8 @@ function validate_password() {
 }
 
 function validate_name_and_surname() {
-    var user_name_input = document.getElementById("name_input_registration_page").value
-    var user_surname_input = document.getElementById("surname_input_registration_page").value
+    var user_name_input = document.getElementById("name_input_registration_page").value;
+    var user_surname_input = document.getElementById("surname_input_registration_page").value;
 
     if (user_name_input.toString() === "") {
         document.getElementById("name_input_registration_page").setAttribute("error", "true");
