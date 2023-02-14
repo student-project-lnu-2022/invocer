8cc3639dfeaef580ad24f9a1d0fcd88dbecaaabd
ostapkilbasovych <ostapkilbasovych@gmail.com	ostapkilbasovych@MacBook-Pro-Ostap ~ %  git config --global user.email ostapkilbasovych@gmail.com>
2023-02-14 14:57:23 +0100

    added const confirmation_text and other requests to change

--- forgotPassword.js
+++ forgotPassword.js
@@ -5,12 +5,14 @@ function onSendCodeClick() {
     document.getElementById("second_step").style.display = "inline";
     const userEmail = document.getElementById("em_input_fp_pg").value;
     const spanUserEmail = document.querySelector("#confirm_txt span");
-    document.getElementById("confirm_txt").style.display = "inline";
-    document.getElementById("confirm_txt").classList.add("animate__animated");
-    document.getElementById("confirm_txt").classList.add("animate__fadeInLeft");
+    const confirmation_text = document.getElementById("confirm_txt");
+    confirmation_text.style.display = "inline";
+    confirmation_text.classList.add("animate__animated");
+    confirmation_text.classList.add("animate__fadeInLeft");
     spanUserEmail.textContent = userEmail;
-    document.getElementById("confirm_btn_fp_pg").addEventListener("click", onCodeConfirmClick);
 }
+
+document.getElementById("confirm_btn_fp_pg").addEventListener("click", onCodeConfirmClick);
 
 function onCodeConfirmClick() {
     document.getElementById("confirm_txt").style.display = "none";
