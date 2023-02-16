0d1006b756dddd60be79dbfb6719ea7bf5b4c2b1
NadiiaNanivska <nanivskan14@gmail.com>
2023-02-16 18:49:32 +0200

    created reusable validation

--- invoicer_project/user/models.py
+++ invoicer_project/user/models.py
@@ -22,15 +22,16 @@ class UserManager(auth_models.BaseUserManager):
         user.save()
         return user
 
+string_validation = [RegexValidator(regex=r'^[A-Z][a-z]+')]
 
 class User(auth_models.AbstractUser):
-    first_name = models.CharField(max_length=35, validators=[RegexValidator(regex=r'^[A-Z][a-z]+')])
-    last_name = models.CharField(max_length=35, validators=[RegexValidator(regex=r'^[A-Z][a-z]+')])
+    first_name = models.CharField(max_length=35, validators=string_validation)
+    last_name = models.CharField(max_length=35, validators=string_validation)
     email = models.EmailField(max_length=35, validators=[RegexValidator(regex=r'^[a-zA-Z0-9.]{3,20}@(?:[a-zA-Z0-9]{2,20}\.){1,30}[a-zA-Z]{2,10}$')], unique=True)
     password = models.CharField(max_length=100, blank=False, validators=[RegexValidator(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,15}$')])
-    company_name = models.CharField(max_length=35, validators=[RegexValidator(regex=r'^[A-Z][a-z]+')])
-    country = models.CharField(max_length=35, validators=[RegexValidator(regex=r'^[A-Z][a-z]+')])
-    city = models.CharField(max_length=35, validators=[RegexValidator(regex=r'^[A-Z][a-z]+')])
+    company_name = models.CharField(max_length=35, validators=string_validation)
+    country = models.CharField(max_length=35, validators=string_validation)
+    city = models.CharField(max_length=35, validators=string_validation)
     address = models.CharField(max_length=40, validators=[RegexValidator(regex=r'^[#./0-9a-zA-Z\s,-]+$')])
     username = None
 
