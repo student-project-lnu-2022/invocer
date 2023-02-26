15148784556874747fc8f04cecbbae3e53ea5d85
KateSydor <85353512+KateSydor@users.noreply.github.com>
2023-02-26 11:36:32 +0200

    Update invoicer_project/clients/urls.py
    Co-authored-by: Maxim <maxshaptala@gmail.com>

--- invoicer_project/clients/urls.py
+++ invoicer_project/clients/urls.py
@@ -9,7 +9,7 @@ delete_client = ClientViewSet.as_view({'delete': 'destroy'})
 urlpatterns = [
     path("home/", TemplateView.as_view(template_name="clients/clients_list.html"), name='home'),
     path("list/", clients_list, name='clients'),
-    path("client/<client_id>", delete_client, name='delete_client'),
+    path("client/<int:id>", delete_client, name='delete_client'),
     path("client/", clients_methods, name='clients_add'), 
     path("add/", TemplateView.as_view(template_name="clients/client_add.html"), name='render_add')
 ]
