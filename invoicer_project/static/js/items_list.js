80f33f5f6303d150c925358e5f7b1fc280646fee
NadiiaNanivska <nanivskan14@gmail.com>
2023-02-28 14:34:23 +0200

    Added html for items list page

---
+++ invoicer_project/static/js/items_list.js
@@ -0,0 +1,21 @@
+const host = "http://127.0.0.1:8000";
+
+document.getElementById("search_bar").addEventListener('keyup', ()=> {
+    let input = document.getElementById('search_bar').value;
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('item_name');
+    let y = document.getElementsByClassName('client_list_item');
+    
+
+    for (let i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            y[i].style.display = "none";
+        } else {
+            y[i].style.removeProperty("display");
+        }
+    }
+});
+
+document.getElementById("adder").addEventListener('click', ()=> {
+    window.location.href = host+'/items/add/';
+})
