f209ae979941e4f866a74729a93be26b21b92061
dmitru4ok <mobilokh4566@gmail.com>
2023-03-04 13:32:07 +0200

    Resolved references to clients and items buttons in sidebar menu

--- invoicer_project/items/urls.py
+++ invoicer_project/items/urls.py
@@ -5,6 +5,6 @@ from django.views.generic import TemplateView
 items_list = ItemViewSet.as_view({"get": "list", "post": "create"})
 
 urlpatterns = [
-    path("list/", TemplateView.as_view(template_name="items/item_list.html"), name='items'),
+    path("list/", TemplateView.as_view(template_name="items/item_list.html"), name='items_render'),
     path("items_list/", items_list, name='items'),
 ]
