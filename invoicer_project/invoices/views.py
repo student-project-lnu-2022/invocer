f8dff0a888cc8f3e3bb4267df80019ef742ecfa8
Vitalik2705 <vitok2misze@gmail.com>
2023-03-22 12:45:41 +0200

    Some changes in database and pdf

--- invoicer_project/invoices/views.py
+++ invoicer_project/invoices/views.py
@@ -61,13 +61,13 @@ class InvoiceViewSet(viewsets.ViewSet):
         p = canvas.Canvas(buffer, pagesize=letter)
         pdfmetrics.registerFont(TTFont('TimesNewRoman', 'static/fonts/Roboto-Regular.ttf'))
         p.setFont('TimesNewRoman', 20)
-        p.drawString(50, 740, f"{serializer.data['name']} від {serializer.data['date_of_payment'].split('-')[2]}.{serializer.data['date_of_payment'].split('-')[1]}.{serializer.data['date_of_payment'].split('-')[0]}р.")
-        p.line(50, 730, 550, 730)
+        p.drawString(40, 740, f"{serializer.data['name']} від {serializer.data['date_of_payment'].split('-')[2]}.{serializer.data['date_of_payment'].split('-')[1]}.{serializer.data['date_of_payment'].split('-')[0]}р.")
+        p.line(40, 730, 565, 730)
         p.setFont('TimesNewRoman', 13)
-        p.drawString(50, 710, f"Постачальник:  {serializer.data['user_first_name']} {serializer.data['user_last_name']}")
-        p.drawString(50, 690, f"Покупець:  {serializer.data['client_first_name']} {serializer.data['client_last_name']}")
+        p.drawString(40, 710, f"Постачальник:  {serializer.data['user_first_name']} {serializer.data['user_last_name']}")
+        p.drawString(40, 690, f"Покупець:  {serializer.data['client_first_name']} {serializer.data['client_last_name']}")
         table_headers = ['№', 'Назва товару', 'К-сть', 'Од.', 'Ціна', 'Сума']
-        table_header_x = 50
+        table_header_x = 40
         table_header_y = 670
         table_row_x = table_header_x
         table_row_y = table_header_y - 30
@@ -75,9 +75,9 @@ class InvoiceViewSet(viewsets.ViewSet):
         id_width = 30
         name_width = 300
         amount_width = 50
-        unit_width = 30
+        unit_width = 40
         price_width = 45
-        sum_width = 50
+        sum_width = 60
 
         p.setFont('TimesNewRoman', 11)
         for i, data in enumerate(items.data):
@@ -126,11 +126,11 @@ class InvoiceViewSet(viewsets.ViewSet):
 
         p.setFont('TimesNewRoman', 14)
         p.drawString(375, table_row_y - 20,f"Всього: {serializer.data['price']} {items.data[0]['item_currency']}")
-        p.line(50, table_row_y - 50, 550, table_row_y - 50)
-        p.drawString(50, table_row_y - 93, f"Від постачальника")
-        p.drawString(300, table_row_y - 93, f"Отримав(ла)")
-        p.line(50, table_row_y - 125, 280, table_row_y - 125)
-        p.line(300, table_row_y - 125, 550, table_row_y - 125)
+        p.line(40, table_row_y - 50, 565, table_row_y - 50)
+        p.drawString(40, table_row_y - 93, f"Від постачальника")
+        p.drawString(310, table_row_y - 93, f"Отримав(ла)")
+        p.line(40, table_row_y - 125, 280, table_row_y - 125)
+        p.line(310, table_row_y - 125, 565, table_row_y - 125)
 
         p.showPage()
         p.save()
