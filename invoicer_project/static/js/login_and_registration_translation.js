23532b433752d5dfbd3dc598825573eb53f69fcd
ostapkilbasovych <ostapkilbasovych@gmail.com	ostapkilbasovych@MacBook-Pro-Ostap ~ %  git config --global user.email ostapkilbasovych@gmail.com>
2023-04-02 13:47:13 +0300

    Added translation to registration page

--- invoicer_project/static/js/login_and_registration_translation.js
+++ invoicer_project/static/js/login_and_registration_translation.js
@@ -50,6 +50,7 @@ i18next.init({
 
 function updateContent() {
     loginTranslation();
+    registrationTranslation();
 }
 
 function getI18NDataFromAttribute(element) {
@@ -82,5 +83,42 @@ function loginTranslation() {
     const createAnAccountHint = document.querySelector(".hint_text_lg_pg.create_account");
     if (createAnAccountHint) {
         createAnAccountHint.textContent = getI18NDataFromAttribute(createAnAccountHint);
+    }
+}
+
+function registrationTranslation() {
+    const signUpText = document.querySelector("#sign_up_text_rg_pg");
+    if (signUpText) {
+        signUpText.textContent = getI18NDataFromAttribute(signUpText);
+    }
+
+    const nameInput = document.querySelector("#name_input_rg_pg");
+    if (nameInput) {
+        nameInput.label = getI18NDataFromAttribute(nameInput);
+    }
+
+    const surnameInput = document.querySelector("#surname_input_rg_pg");
+    if (surnameInput) {
+        surnameInput.label = getI18NDataFromAttribute(surnameInput);
+    }
+
+    const passwordInput = document.querySelector("#password_input_rg_pg");
+    if (passwordInput) {
+        passwordInput.label = getI18NDataFromAttribute(passwordInput);
+    }
+
+    const repeatPasswordInput = document.querySelector("#repeat_password_input_rg_pg");
+    if (repeatPasswordInput) {
+        repeatPasswordInput.label = getI18NDataFromAttribute(repeatPasswordInput);
+    }
+
+    const createProfileButton = document.querySelector("#sign_up_confirm_btn_rg_pg");
+    if (createProfileButton) {
+        createProfileButton.label = getI18NDataFromAttribute(createProfileButton);
+    }
+
+    const hintHaveAccount = document.querySelector(".hint_text_rg_pg");
+    if (hintHaveAccount) {
+        hintHaveAccount.textContent = getI18NDataFromAttribute(hintHaveAccount);
     }
 }
