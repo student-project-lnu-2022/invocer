7e7ca7c7f89c238581ee81d380cd06d25127d6c2
KateSydor <katasydor@gmail.com>
2023-04-07 18:14:27 +0300

    Add settings page

--- invoicer_project/user/urls.py
+++ invoicer_project/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegistrationViewSet, LoginViewSet, RefreshViewSet
+from .views import RegistrationViewSet, LoginViewSet, RefreshViewSet, UserSettingsViewSet
 from django.urls import path
 from django.views.generic import TemplateView
 
@@ -8,6 +8,7 @@ login_methods = LoginViewSet.as_view({'get': 'login_page', 'post': 'create'})
 refresh_methods = RefreshViewSet.as_view({'post': 'create'})
 decode_methods = RefreshViewSet.as_view({'post': 'decode'})
 logout_method = LoginViewSet.as_view({'post': 'logout'})
+settings_method = UserSettingsViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update'})
 
 urlpatterns = [
     path("register/", register_methods, name='register'),
@@ -15,5 +16,7 @@ urlpatterns = [
     path('refresh/', refresh_methods , name='refresh'),
     path('decode/', decode_methods , name='decode'),
     path('forgot_password/', TemplateView.as_view(template_name="user/forgot_password.html") , name='forgot_password'),
-    path('logout/', logout_method, name='logout')
+    path('logout/', logout_method, name='logout'),
+    path('settings/', TemplateView.as_view(template_name="user/settings.html"), name='settings'),
+    path("user/", settings_method),
 ]
