a28bf94f5d4f58c4badb37c66e086598c8fa83d3
ostapkilbasovych <ostapkilbasovych@gmail.com	ostapkilbasovych@MacBook-Pro-Ostap ~ %  git config --global user.email ostapkilbasovych@gmail.com>
2023-04-07 14:21:10 +0300

    Fixed some overflows in edit and add client page

--- invoicer_project/static/js/items_list_request.js
+++ invoicer_project/static/js/items_list_request.js
@@ -17,10 +17,10 @@ function createItemListContent(data) {
 
         document.getElementById("items_container").insertAdjacentHTML('afterbegin', `
             <div class="row items_list_item align-items-center justify-content-around redirect_to_item_info" data-item-id="${itemID}">
-                <div class="col-xxl-5 col-xl-5 col-md-4 col-sm-2 col-2 list_item_name redirect_to_item_info list_item_name redirect_to_item_info" data-item-id="${itemID}">
+                <div class="col-xxl-5 col-xl-5 col-md-4 col-sm-2 col-3 list_item_name redirect_to_item_info list_item_name redirect_to_item_info" data-item-id="${itemID}">
                     <p class="item_name redirect_to_item_info" data-item-id="${itemID}">${itemName}</p>
                 </div>
-                <div class="d-flex flex-wrap flex-row justify-content-end col-xxl-7 col-xl-7 col-md-8 col-sm-8 col-8 redirect_to_item_info" data-item-id="${itemID}">
+                <div class="d-flex flex-wrap flex-row justify-content-end col-xxl-7 col-xl-7 col-md-8 col-sm-8 col-7 redirect_to_item_info" data-item-id="${itemID}">
                     <div class="d-flex flex-wrap flex-column list_item_info_block redirect_to_item_info" data-item-id="${itemID}">
                         <p class="additional_text redirect_to_item_info" data-item-id="${itemID}">Price per unit</p>
                         <p class="main_text redirect_to_item_info" data-item-id="${itemID}">${priceAndCurrency}</p>
