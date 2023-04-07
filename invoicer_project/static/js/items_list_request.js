15530438c5328fcc39c7447f3fcb09b22ecb4c56
ostapkilbasovych <ostapkilbasovych@gmail.com	ostapkilbasovych@MacBook-Pro-Ostap ~ %  git config --global user.email ostapkilbasovych@gmail.com>
2023-04-07 12:28:09 +0300

    Added more button to list item

--- invoicer_project/static/js/items_list_request.js
+++ invoicer_project/static/js/items_list_request.js
@@ -15,29 +15,39 @@ function createItemListContent(data) {
         let priceAndCurrency = item['price'] + " " + item['currency'];
         let itemID = item['id']
 
-        document.getElementById("items_container").insertAdjacentHTML('afterbegin', `<div class="row items_list_item align-items-center justify-content-around redirect_to_item_info" data-item-id="${itemID}">
-                        <div class="col-md-4 col-sm-3 col-3 list_item_name redirect_to_item_info list_item_name redirect_to_item_info" data-item-id="${itemID}">
-                            <p class="item_name redirect_to_item_info" data-item-id="${itemID}">${itemName}</p>
-                        </div>
-                        <div class="d-flex flex-wrap flex-row justify-content-end col-md-8 col-sm-9 col-9 redirect_to_item_info" data-item-id="${itemID}">
-                            <div class="d-flex flex-wrap flex-column list_item_info_block redirect_to_item_info" data-item-id="${itemID}">
-                                <p class="additional_text redirect_to_item_info" data-item-id="${itemID}">Price per unit</p>
-                                <p class="main_text redirect_to_item_info" data-item-id="${itemID}">${priceAndCurrency}</p>
-                            </div>
-                            <div class="d-flex flex-wrap flex-column list_item_info_block redirect_to_item_info" data-item-id="${itemID}">
-                                <p class="additional_text redirect_to_item_info" data-item-id="${itemID}">Basic unit</p>
-                                <p class="main_text redirect_to_item_info" data-item-id="${itemID}">kg</p>
-                            </div>
-                            <div class="list_item_user_buttons">
-                                 <md-standard-icon-button class="edit-item" data-element-id="${itemID}"><span class="material-symbols-outlined">edit</span></md-standard-icon-button>
-                                <md-standard-icon-button class="delete-item" data-element-id="${itemID}"><span class="material-symbols-outlined">delete</span></md-standard-icon-button>
-                                <md-checkbox class="delete_items_checkbox" id="list_item_user_delete"  data-element-id="${itemID}"></md-checkbox>
-                            </div>
-                        </div>
-                    </div>`)
+        document.getElementById("items_container").insertAdjacentHTML('afterbegin', `
+            <div class="row items_list_item align-items-center justify-content-around redirect_to_item_info" data-item-id="${itemID}">
+                <div class="col-md-4 col-sm-3 col-3 list_item_name redirect_to_item_info list_item_name redirect_to_item_info" data-item-id="${itemID}">
+                    <p class="item_name redirect_to_item_info" data-item-id="${itemID}">${itemName}</p>
+                </div>
+                <div class="d-flex flex-wrap flex-row justify-content-end col-md-8 col-sm-9 col-9 redirect_to_item_info" data-item-id="${itemID}">
+                    <div class="d-flex flex-wrap flex-column list_item_info_block redirect_to_item_info" data-item-id="${itemID}">
+                        <p class="additional_text redirect_to_item_info" data-item-id="${itemID}">Price per unit</p>
+                        <p class="main_text redirect_to_item_info" data-item-id="${itemID}">${priceAndCurrency}</p>
+                    </div>
+                    <div class="d-flex flex-wrap flex-column list_item_info_block redirect_to_item_info" data-item-id="${itemID}">
+                        <p class="additional_text redirect_to_item_info" data-item-id="${itemID}">Basic unit</p>
+                        <p class="main_text redirect_to_item_info" data-item-id="${itemID}">kg</p>
+                    </div>
+                    <div class="list_item_more_button">
+                        <md-standard-icon-button class="more-item" data-element-id="${itemID}" data-contextmenu="item-context-menu-${itemID}">
+                            <span class="material-symbols-outlined">more_vert</span>
+                        </md-standard-icon-button>
+                    </div>
+                    <div class="list_item_user_buttons">
+                        <md-standard-icon-button class="edit-item" data-element-id="${itemID}">
+                            <span class="material-symbols-outlined">edit</span>
+                        </md-standard-icon-button>
+                        <md-standard-icon-button class="delete-item" data-element-id="${itemID}">
+                            <span class="material-symbols-outlined">delete</span>
+                        </md-standard-icon-button>
+                        <md-checkbox class="delete_items_checkbox" id="list_item_user_delete" data-element-id="${itemID}"></md-checkbox>
+                    </div>
+                </div>
+            </div>
+        `);
     }
 }
-
 
 async function addElementsDynamically() {
     let responseFromServer = await getUserData('/items/items_list/');
